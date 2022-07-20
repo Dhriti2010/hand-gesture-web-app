2c413bc427112f2cb123738bc2c649592f2d7da1
Dhriti2010 <89203702+Dhriti2010@users.noreply.github.com>
2022-07-20 16:49:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -17,3 +17,37 @@ classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/5
 function modelloaded(){
     console.log("model is loaded");
 }
+function check(){
+    img=document.getElementById("captured_image");
+    classifier.classify(img,gotresults);
+}
+function gotresults(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        
+      
+        prediction1=results[0].label;
+        prediction2=results[1].label;
+        
+        if (prediction1=="amazing"){
+            document.getElementById("result_emoji").innerHTML="&#128076";
+            document.getElementById("result_emotion").innerHTML="Amazing";
+        }
+        if (prediction1=="ok"){
+            document.getElementById("result_emoji").innerHTML="&#128077";
+            document.getElementById("result_emotion").innerHTML="All the best/ok";
+        }
+        if (prediction1=="peace"){
+            document.getElementById("result_emoji").innerHTML="&#9996";
+            document.getElementById("result_emotion").innerHTML="Peace/Victory"; 
+        }  
+        if (prediction1=="yo"){
+            document.getElementById("result_emoji").innerHTML="&#129304";
+            document.getElementById("result_emotion").innerHTML="yo";
+        }             
+    }
+}
+    
